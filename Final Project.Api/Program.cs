e474fc225d2388749fa92fece13da6aa81b8edf6
Amr khaled <Amr khaled@DESKTOP-551V1S8>
2024-12-05 19:42:48 +0200

    create step1

--- Final Project.Api/Program.cs
+++ Final Project.Api/Program.cs
@@ -16,7 +16,7 @@ namespace FinalProject.Api
     {//dev
         public static void Main(string[] args)
         {
-            //dev
+            //step1
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
